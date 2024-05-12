768883cee5ee46d445d4e17dd15f33082969f6f1
Prajal190904 <pranjalvyas45@gmail.com>
2024-05-12 17:13:10 +0530

    erroe resolving

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -7,7 +7,7 @@ const projectUpload=async(req,res)=>{
         const student=await Student.findOne({email:email});
         if(!student)
         {
-            return res.status(404).json({message:"Student not found"});
+            return res.status(404).json({message:"Email not Registered"});
         }
         if(req.file.size>1024*1024*10)
         {

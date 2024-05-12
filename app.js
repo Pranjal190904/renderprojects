f3ec537c181140f40e8ca2d86df0fa9ad3b294c5
Prajal190904 <pranjalvyas45@gmail.com>
2024-05-12 12:33:59 +0530

    cors config

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app=express();
 const {port}=require('./config/env.config');
 const router=require('./routers/project.routes');
 const connectDB=require('./config/db.config');
+const handleCors=require('./config/cors.config');
 
 connectDB();
 app.use(express.json());
+app.use(handleCors);
 app.use('/api/render',router);
 
 app.listen(port);
---
+++ config/cors.config.js
@@ -0,0 +1,11 @@
+const cors = require("cors");
+
+const corsOptions = {
+  origin : '*',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+const handleCors = cors(corsOptions);
+
+module.exports = handleCors;

e677385255147399eb3a6f1c6f82e1f8de149afa
Prajal190904 <pranjalvyas45@gmail.com>
2024-05-14 23:09:07 +0530

    error resolving

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -27,6 +27,7 @@ const projectUpload=async(req,res)=>{
         return res.status(200).json({message:"Project uploaded successfully"});
     }
     catch(err){
+        console.log(err);
         return res.status(500).json({error:"Internal server error"});
     }
 }

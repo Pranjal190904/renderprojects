0272da28b5979404e48aa8bb6f8ee6281848c03a
Prajal190904 <pranjalvyas45@gmail.com>
2024-05-14 22:54:36 +0530

    cors config

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin : '*',
+  origin : ['https://render-2-0-project-submission.vercel.app/','http://localhost:5173'],
   credentials: true,
   optionsSuccessStatus: 200,
 };
